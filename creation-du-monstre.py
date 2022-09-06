e01cbce53d11a8c20fdd34c425af6f288a44ed63
romeroafonso <romero.afonso@labom2iformation.fr>
2022-09-06 11:00:16 +0200

    changement du nom des fichiers pour supprimer -

--- creation-du-monstre.py
+++ creation-du-monstre.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env/python3
-from generation-du-monstre.py import generation_du_monste
+from generation_du_monstre import generation_du_monstre
 def creation_du_monstre():
     nom_monstre=input('veuillez saisir le nom du monstre: ')
     
