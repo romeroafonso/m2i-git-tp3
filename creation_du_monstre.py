f6dee68abb2cd87de213f27117a43c22e661e822
helene michel <helenemichel59@gmail.com>
2022-09-06 11:26:50 +0200

    creation du monstre

--- creation_du_monstre.py
+++ creation_du_monstre.py
@@ -2,9 +2,6 @@
 from generation_du_monstre import generation_du_monstre
 def creation_du_monstre():
     nom_monstre=input('veuillez saisir le nom du monstre: ')
-    
-    monstre=[nom_monstre]
-#coucou
-    monstre.append(generation_du_monstre())
-
-print(creation_du_monstre())
+    monstre=generation_du_monstre()
+    monstre.insert(0, nom_monstre)
+    return(monstre)
