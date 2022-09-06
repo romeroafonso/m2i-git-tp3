8a98c644a9a39348e3fd73ea25dd19f81452cf2b
romeroafonso <romero.afonso@labom2iformation.fr>
2022-09-06 14:27:03 +0200

    load gestion des enemis tués

---
+++ compteur_ennemis_tues.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Code pour compter les ennemies tués
+import gestion_du_combat as gc
+def compteur(gc[1], compteur_victoires):
+
+    compteur_victoires = 0
+    if pv_perso > 0:
+        compteur_victoires += 1
+    return(compteur_victoires)
