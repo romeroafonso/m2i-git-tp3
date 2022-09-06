36c8aa334752b4959aba3c1bd1a928e47a7b501b
romeroafonso <romero.afonso@labom2iformation.fr>
2022-09-06 14:43:38 +0200

    compteur enemis tues - v2

--- compteur_ennemis_tues.py
+++ compteur_ennemis_tues.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 # Code pour compter les ennemies tués
-import gestion_du_combat as gc
-def compteur(gc[1], compteur_victoires):
-
+from gestion_des_dégâts import gestion_des_degats as gd
+def compteur(pv_perso, compteur_victoires):
     compteur_victoires = 0
     if pv_perso > 0:
         compteur_victoires += 1
+        print(compteur_victoires)
     return(compteur_victoires)
