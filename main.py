b01ebc68ad274630118749b0f33116eb8b88ca16
helene michel <helenemichel59@gmail.com>
2022-09-09 13:22:12 +0200

    projet final jeu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python3
 
 from flask import Flask, render_template, request, send_file, redirect, url_for, Response, redirect
+from lejeu.jeumain import jeu
 
 app = Flask(__name__)
 
@@ -10,10 +11,13 @@ import os
 
 @app.route('/', methods=['GET', 'POST'])
 def menu():
-    if request.form:
-        print()
+    pseudo=None
     monstres=[]
-    return render_template("index.html", monstres=monstres)
+    if request.form:
+        pseudo=request.form["nom"]
+        print(request.form["nom"])
+        monstres=jeu(pseudo)
+    return render_template("index.html", monstres=monstres, pseudo=pseudo, nbr_monstres=len(monstres))
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8001)
