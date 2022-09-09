4969d0ca11c9f74a6205dc09e5b5c0342087ea24
romeroafonso <romero.afonso@labom2iformation.fr>
2022-09-09 15:24:27 +0200

    préparation avant mise en service OC

--- compteur_ennemis_tues.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-# Code pour compter les ennemies tués
-def compteur(pv_perso):
-    compteur_victoires = 0
-    if pv_perso > 0:
-        compteur_victoires += 1        
-    return(compteur_victoires)
